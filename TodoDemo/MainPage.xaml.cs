0376cd1681fbbcd5354b7aaa0be4b7170f39fe82
Vedant Karande <vedant.karande@samcomtechnobrains.com>
2024-07-30 11:05:27 +0530

    MAUI Todo Demo Completed

--- TodoDemo/MainPage.xaml.cs
+++ TodoDemo/MainPage.xaml.cs
@@ -1,25 +1,42 @@
-﻿namespace TodoDemo
+﻿using System.Collections.ObjectModel;
+
+namespace TodoDemo
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+            TodoItems = new ObservableCollection<TodoItem>();
+            TodoListView.ItemsSource = TodoItems;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        //Add Functionality
+        private void OnSubmitClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            if (!string.IsNullOrEmpty(TodoEntry.Text))
+            {
+                TodoItems.Add(new TodoItem { Task = TodoEntry.Text });
+                TodoEntry.Text = string.Empty;
+            }
         }
+
+        //Delete Functionality
+        private void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var item = button?.CommandParameter as TodoItem;
+            if (item != null)
+            {
+                TodoItems.Remove(item);
+            }
+        }
+    }
+    public class TodoItem
+    {
+        public string Task { get; set; }
     }
 
 }
